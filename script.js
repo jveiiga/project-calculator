3fed5fb6f4c5fd92043e074d2bbda208c84d0cbb
Jeferson Veiga <jeferson.veiiga@icloud.com>
2022-03-09 15:14:31 -0300

    new-features

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-
-function displaynum(n1) {
-    Calculator.text1.value = Calculator.text1.value + n1;
+function adicionar_parametro(parametro) {
+    document.querySelector("[name='tela']").value += parametro
 }
+
+function calcular() {
+    conta = document.querySelector("[name='tela']").value
+    document.querySelector("[name='tela']").value = eval(conta)
+}
+
+function porcentagem() {
+    conta  = document.querySelector("[name='tela']").value + '/100'
+    document.querySelector("[name='tela']").value = eval(conta)
+}
+
+function botao_AC() {
+    document.querySelector("[name='tela']").value = ''
+}
+
